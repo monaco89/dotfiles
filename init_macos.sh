15639886dda2a588c49a8e6b0516381c478b71a1
monaco89 <nick.monaco15@gmail.com>
2021-09-29 17:22:36 -0400

    update all configs

--- init_macos.sh
+++ init_macos.sh
@@ -8,73 +8,100 @@ xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew analytics off
 
-echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
 
-# Utilities
-brew install \
-    bash-completion `: autocompletion for bash terminal` \
-    htop            `: better top` \
-    iftop           `: top for network I/O` \
-    jq              `: parse and prettify json` \
-    ngrep           `: read network traffic` \
-    nmap            `: network map` \
-    tree            `: recursive ls` \
-    wget            `: curl alternative` \
-    vim             `: install vim 8.0` \
-
-
-echo "[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion" >> ~/.bash_profile
-
-source ~/.bash_profile
+# Install jq for parsing JSON
+brew install jq
 
 brew install git
 
-# Python
+# tmux
+brew install tmux
+
+# Install Python
 brew install python
 brew install python3
 
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+brew install php
+brew install gmp
+
+# Install some CTF tools; see https://github.com/ctfs/write-ups.
+brew install aircrack-ng
+brew install bfg
+brew install binutils
+brew install binwalk
+brew install cifer
+brew install dex2jar
+brew install dns2tcp
+brew install fcrackzip
+brew install foremost
+brew install hashpump
+brew install hydra
+brew install john
+brew install knock
+brew install netpbm
+brew install nmap
+brew install pngcheck
+brew install socat
+brew install sqlmap
+brew install tcpflow
+brew install tcpreplay
+brew install tcptrace
+brew install ucspi-tcp # `tcpserver` etc.
+brew install xpdf
+brew install xz
+
+# Install some developer tools
+# Serverless
+npm install -g serverless
+# gatsby cli
+npm install -g gatsby-cli
+# Postgresql
+brew install postgresql
+initdb /usr/local/var/postgres
+# Mysql
+brew install mysql
+brew cask install mysqlworkbench
+# Vscode
+brew cask install visual-studio-code
+# Iterm2
+brew cask install iterm2
+# Bash git-prompt
+brew install bash-git-prompt
+# Mailhog
+brew install mailhog
+# Composer & yarn
+brew install composer yarn
+# Node.js
+brew install node
+# AWS CLI
+brew install awscli
+
+# Install misc programs
+brew install --cask 1password
+brew install --cask tiles
+brew install --cask insomnia
+brew install --cask brave-browser
+brew install --cask firefox
+
+# Install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 pip install --upgrade setuptools
 pip install --upgrade pip
-
-brew install gnu-sed --with-default-names
-
-# Heroku
-brew install heroku-toolbelt
-heroku update
-heroku login
-mkdir ~/.ssh
-ssh-keygen -t rsa
-heroku keys:add
-
-# Postgresql
-brew install postgresql
-initdb /usr/local/var/postgres
-
-# Mysql
-brew install mysql
-brew cask install mysqlworkbench
-
-# Vscode
-brew cask install visual-studio-code
-
-# Iterm2
-brew cask install iterm2
-
-# Bash git-prompt
-brew install bash-git-prompt
-
-# Mailhog
-brew install mailhog
-
-brew cask install kdiff3
-
-# Composer & yarn
-brew install composer yarn
-
-# Node.js
-brew install node
 
 # Git configurations
 #git config --global user.name
@@ -90,45 +117,20 @@ git clone https://github.com/square/maximum-awesome.git
 cd maximum-awesome
 rake
 
-# ohmyzsh: https://github.com/ohmyzsh/ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# https://github.com/agnoster/agnoster-zsh-theme
-# TODO Change .zshrc line to ZSH_THEME="agnoster"
-# install powerlines/f
-# clone
-cd ~/
-git clone https://github.com/powerline/fonts.git --depth=1
-# install
-cd fonts
-./install.sh
-# clean-up a bit
-cd ..
-rm -rf fonts
-
-# or https://medium.com/@genealabs/agnoster-theme-on-os-x-391d60effaf6
+# Install powerlevel10k for OhMyZsh
+brew install romkatv/powerlevel10k/powerlevel10k
+echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+# pk10k configure
 
 brew install zsh-syntax-highlighting
-
 echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-
-# https://github.com/altercation/solarized/tree/master/iterm2-colors-solarized
-
-# TODO https://github.com/zsh-users/zsh-completions
-# TODO https://github.com/djui/alias-tips
-
-echo "prompt_context() {
-  if [[ "$USER" != "$DEFAULT_USER" || -n "$SSH_CLIENT" ]]; then
-    # Custom (Random emoji)
-    emojis=(" ️" "🔥" "💀" "👑" "😎" "🐸" "🐵" "🦄" "🌈" "🍻" "🚀" "💡" "🎉" "🔑" "🇹🇭" "🚦" "🌙")
-    RAND_EMOJI_N=$(( $RANDOM % ${#emojis[@]} + 1))
-    prompt_segment black default "${emojis[$RAND_EMOJI_N]} %(!.%{%F{yellow}%}.)$USER"
-  fi
-}" >> ~/.zshrc
 
 # Install python 3.x
 brew install pyenv
 brew install zlib
 brew install python
+# install poetry
+brew install poetry
 # Install pipx to manage global packages
 python3 -m pip install --user pipx
 python3 -m userpath append ~/.local/bin
@@ -142,9 +144,6 @@ pyenv install 3.7.3
 pyenv global 3.7.3
 
 echo "alias python=/usr/local/bin/python3.7" >> ~/.zshrc
-
-# install poetry
-curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
 
 # TODO Fix
 poetry config virtualenvs.in-project true
@@ -174,20 +173,8 @@ gpip3(){
    PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
 }" >> ~/.zshrc
 
-# MongoDB
-brew tap mongodb/brew
-brew install mongodb-community
-brew services start mongodb-community
+# Remove outdated versions from the cellar.
+brew cleanup
 
-# tmux
-brew install tmux
-
-# Serverless
-npm install -g serverless
-
-# gatsby cli
-npm install -g gatsby-cli
-
-# cocoapods
-sudo gem install cocoapods
-pod setup
+# Install knex.js
+yarn global add knex
